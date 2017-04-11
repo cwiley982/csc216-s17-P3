63baf426a11c8bd2d35bf89e2ca3de1017289388
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-04-11 10:43:25 -0400

    Added more javadoc to a few ui classes

--- Project3/src/edu/ncsu/csc216/bbtp/ui/TestCaseListPane.java
+++ Project3/src/edu/ncsu/csc216/bbtp/ui/TestCaseListPane.java
@@ -8,6 +8,13 @@ import javax.swing.JTable;
 
 import edu.ncsu.csc216.bbtp.model.TestCaseList;
 
+/**
+ * Holds a list of test cases and puts them into a table to be displayed to the
+ * user
+ * 
+ * @author Caitlyn
+ *
+ */
 public class TestCaseListPane implements Serializable, Observer {
 
 	private static final long serialVersionUID = -2210716111020406799L;
@@ -16,26 +23,58 @@ public class TestCaseListPane implements Serializable, Observer {
 	private int[] colWidths;
 	private TestCaseTableModel tctm;
 
+	/**
+	 * Constructs a TestCaseListPane with the given list of test cases
+	 * 
+	 * @param list
+	 *            the list of test cases to populate the pane with
+	 */
 	public TestCaseListPane(TestCaseList list) {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Gets the test case table model holding the data for test cases in a 2D
+	 * array
+	 * 
+	 * @return tctm - defined above
+	 */
 	public TestCaseTableModel getTestCaseTableModel() {
 		return null;
 	}
 
+	/**
+	 * Returns a JTable containing the info for the test cases
+	 * 
+	 * @return a JTable with test case info
+	 */
 	public JTable getTable() {
 		return null;
 	}
 
+	/**
+	 * Initializes the view by creating the JTable and associating it with the
+	 * TestCaseTableModel
+	 */
 	private void initView() {
 
 	}
 
+	/**
+	 * Clears the current data
+	 */
 	public void clearSelection() {
 
 	}
 
+	/**
+	 * Notifies the observers when a change is made
+	 * 
+	 * @param observable
+	 *            the action being observed
+	 * @param obj
+	 *            the object (observer) to be notified of the change
+	 */
 	public void update(Observable observable, Object obj) {
 
 	}
--- Project3/src/edu/ncsu/csc216/bbtp/ui/TestCaseTableModel.java
+++ Project3/src/edu/ncsu/csc216/bbtp/ui/TestCaseTableModel.java
@@ -2,6 +2,12 @@ package edu.ncsu.csc216.bbtp.ui;
 
 import java.io.Serializable;
 
+/**
+ * Contains the data displayed in the TestCaseListPane's JTable
+ * 
+ * @author Caitlyn
+ *
+ */
 public class TestCaseTableModel implements Serializable {
 
 	private static final long serialVersionUID = 5954551753060998701L;
@@ -10,34 +16,91 @@ public class TestCaseTableModel implements Serializable {
 	/** 2D array containing all the data in the table */
 	private Object[][] data;
 
+	/**
+	 * Constructs a TestCaseTableModel using the information in the 2D array
+	 * passed in
+	 * 
+	 * @param testCases
+	 *            the 2D array with test case data
+	 */
 	public TestCaseTableModel(Object[][] testCases) {
 
 	}
 
+	/**
+	 * Gets the number of rows in the table
+	 * 
+	 * @return number of rows
+	 */
 	public int getRowCount() {
 		return -1;
 	}
 
+	/**
+	 * Gets the number of columns in the table
+	 * 
+	 * @return number of columns
+	 */
 	public int getColumnCount() {
 		return -1;
 	}
 
+	/**
+	 * Gets the name of the column
+	 * 
+	 * @return name of the column
+	 */
 	public String getColumnName() {
 		return "";
 	}
 
+	/**
+	 * Returns the data being held at the row and column index given
+	 * 
+	 * @param row
+	 *            row to get data from
+	 * @param col
+	 *            column to get data from
+	 * @return data at position with row and column equal to given values
+	 */
 	public Object getValueAt(int row, int col) {
 		return null;
 	}
 
+	/**
+	 * Sets the data at the given row and column number to the specified object
+	 * 
+	 * @param obj
+	 *            object to set data to
+	 * @param row
+	 *            row to consider
+	 * @param col
+	 *            column to consider
+	 */
 	public void setValueAt(Object obj, int row, int col) {
 
 	}
 
+	/**
+	 * Returns all the data in the row specified, all data is for one test case
+	 * 
+	 * @param row
+	 *            the row to get data for
+	 * @return TestCaseData object containing the test case's info
+	 */
 	public TestCaseData getTestCaseRowData(int row) {
 		return null;
 	}
 
+	/**
+	 * Sets the data in the row specified to the data in the TestCaseData
+	 * object, writes over current row data
+	 * 
+	 * @param row
+	 *            the row to set the data for
+	 * @param data
+	 *            the TestCaseData object to get the data from
+	 */
 	public void setTaskRowData(int row, TestCaseData data) {
 
 	}
